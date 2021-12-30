ca83d398a578a7564b7f3c587ce254ad4ab29504
Alexey Tolkov <atolkov.ext@igus.net>
2021-12-30 15:49:46 +0100

    mob next [ci-skip] [ci skip] [skip ci]

---
+++ Christmastree.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace christmas_tree_kata
+{
+    internal class Christmastree
+    {
+        internal static object Draw(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
